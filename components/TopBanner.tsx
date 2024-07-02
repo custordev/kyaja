3331e3098ea9e643976b5e5ac8cc3624fdcd0472
Ian Sebugenyi <140151921+Iancustor@users.noreply.github.com>
2024-07-02 17:16:13 +0300

    changed the top banner

--- components/TopBanner.tsx
+++ components/TopBanner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function TopBanner() {
   return (
     <div className="bg-[#633185] flex items-center justify-center">
-      <img className="w-auto h-auto" src="/gif/kyaja.gif" alt="Kyaja gif" />
+      <img src="/gif/kyaja.gif" alt="" className=" object-cover rounded-md" />
     </div>
   );
 }
